b1a5957deaa02a224eac27ae8a1cb7ae1b942c0d
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-08-31 00:47:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, inputName) {
+    let sameName = drivers.filter(driver => {
+        return driver === inputName || driver.includes(inputName.charAt(0).toLowerCase());
+    })
+    return sameName
+}
+
+function fuzzyMatch(drivers, inputName) {
+    let sameLetters = drivers.filter(driver => {
+        return driver.startsWith(inputName.charAt(0));
+    })
+    return sameLetters
+}
+
+function matchName(drivers, inputName) {
+    let getName = drivers.filter(driver => {
+        return driver.name === inputName;
+    })
+    return getName
+}
